786e5a97a6e4b0d93eb7d059fd0b505344bd6026
João Seródio <joaoserodio@live.com>
2023-04-26 00:40:20 -0300

    cmp_logs.py minor improvements

--- cmp_logs.py
+++ cmp_logs.py
@@ -5,4 +5,8 @@ if __name__ == "__main__":
     log_files = sorted(glob("test/*.log"))
     log_files = [f.split("/")[-1] for f in log_files]
     a = filecmp.cmpfiles("spike/outputs", "test", log_files)
-    print(a)
+    print(f"Matching Logs: {len(a[0])}/{len(log_files)}")
+    if len(a[1]) != 0:
+        print(f"These files do not match: {a[1]}")
+    if len(a[2]) != 0:
+        print(f"These files do not have a corresponding pair: {a[2]}")
